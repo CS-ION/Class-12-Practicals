2402ffa3662755c5ba9597750d0b976c223deccf
Vivek Ashok <37447267+codephile1221@users.noreply.github.com>
2021-10-11 23:15:21 +0530

    Update 1.py

--- Practicals/1.py
+++ Practicals/1.py
@@ -1,7 +1,5 @@
-def pig_latin(word):
-    size = len(word)
-    if word[size-2:size:1] != 'ay':
-        return word
-    return word[size-3] + word[:size-3:]
-x = input('enter the word : ')
-print(f'the word is {pig_latin(x)}')
+def piglatin(string):
+        return string[-3] + string[:-3] if string[-2:] == 'ay' else string
+
+word = input()
+print(piglatin(word))
