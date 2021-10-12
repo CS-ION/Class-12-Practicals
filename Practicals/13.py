b16215577ead445abfb0bee6ef6b6ee272ff6368
Vivek Ashok <37447267+codephile1221@users.noreply.github.com>
2021-10-12 21:51:18 +0530

    Update 13.py
    Made changes as per the new requirements.

--- Practicals/13.py
+++ Practicals/13.py
@@ -1,24 +1,16 @@
-f = open('Sentence.txt','r')
-for I in f.readlines():
-    size = len(I)
-    if I[size-2] in ['.','?','!']:
-        pot_sen = []
-        pot_val = []
-        print(f'Sentence : {I[0:size-1:1]}')
-        for J in I[0:size-2:1].split(' '):
-            ascival = 0
-            for K in J:
-                ascival += ord(K)
-            print(f'{J} = {ascival}')
-            pot_sen.append(f'{J} {ascival}')
-            pot_val.append(ascival)
-        new_sen = ''
-        for M in sorted(pot_val):
-            for N in pot_sen:
-                if str(M) in N:
-                    new_sen += N.split(' ')[0]
-                    break
-            new_sen += ' '
-        print(f'Highest Potential Sentence : {new_sen}\n')
-    else:
-        print(f"Invalid Sentence : {I}")
+
+def potential(word):
+    return sum([ord(c) for c in word])
+
+
+with open('resources/Sentence.txt', 'r') as sentence_file:
+    content = sentence_file.read()
+    for letter in content:
+        if letter in ('.', '?', '!'):
+            sentence = content.split(letter)[0].strip()
+            print(sentence + letter)
+
+            words = sentence.split()
+            decoded_sentence = ' '.join(sorted(words, key=potential))
+            print(decoded_sentence)
+            break
